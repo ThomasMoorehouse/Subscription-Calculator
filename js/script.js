d885f14bb9d34ad742f202e652fe5d3d839faa02
thomas_m <moorehousetreasures@gmail.com>
2024-01-11 21:03:08 -0500

    Added select elements

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 // Your JS code here
+
+// select elements
+var subTypeElement = document.querySelector("#subscription") ;
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+// change event listeners
+subTypeElement.addEventListener("change", function (e){
+
+})
