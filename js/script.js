7cda834277d7aa7bb3661d1519ef597a3f8e9d0d
thomas_m <moorehousetreasures@gmail.com>
2024-01-12 22:17:33 -0500

    Added function to calculate the cost

--- js/script.js
+++ js/script.js
@@ -10,12 +10,25 @@ var subDuration = 1;
 // change event listeners
 subTypeElement.addEventListener("change", function (e){
     subType = e.target.value
-    console.log(subType);
+    //console.log(subType);
+    updateSubscriptionDiv()
 });
 
 subDurationElement.addEventListener("change", function (e){
     Number(subDuration = e.target.value)
-    console.log(subDuration);
+    //console.log(subDuration);
+    updateSubscriptionDiv()
 });
 
 // Use a Function to Calculate the Cost
+
+var updateSubscriptionDiv = function () {
+    monthlyCost = 5
+    if (subType === "standard") {
+        monthlyCost = 7    
+    } else if (subType === "premium") {
+        monthlyCost = 10
+    }
+    var total = subDuration * monthlyCost
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for ${total}.`
+};
