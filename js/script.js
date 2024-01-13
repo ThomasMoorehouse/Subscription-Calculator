50e21b1c247139a957facc8b9c166bc2cf50a74f
thomas_m <moorehousetreasures@gmail.com>
2024-01-12 21:33:20 -0500

    Added change event listeners

--- js/script.js
+++ js/script.js
@@ -9,5 +9,13 @@ var subDuration = 1;
 
 // change event listeners
 subTypeElement.addEventListener("change", function (e){
+    subType = e.target.value
+    console.log(subType);
+});
 
-})
+subDurationElement.addEventListener("change", function (e){
+    Number(subDuration = e.target.value)
+    console.log(subDuration);
+});
+
+// Use a Function to Calculate the Cost
